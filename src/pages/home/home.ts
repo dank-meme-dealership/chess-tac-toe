24fa81d9d3fb8c976305ec39987f9477b97b9aff
Matttaylor8910 <taylor.matt777@gmail.com>
2018-04-04 09:42:25 -0600

    Simple nav to gameplay page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { GameplayPage } from '../gameplay/gameplay'
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,6 +11,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  goToGameplay() {
+    this.navCtrl.push(GameplayPage);
   }
 
 }
