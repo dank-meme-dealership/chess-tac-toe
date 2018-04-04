85969af171e25aa6892c49812acdcc81ea127249
Matttaylor8910 <taylor.matt777@gmail.com>
2018-04-04 15:34:07 -0600

    Add user to firebase and persist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import {ComponentsModule} from "../components/components.module";
 import {AngularFireModule} from 'angularfire2';
 import {AngularFireDatabaseModule, AngularFireDatabase} from 'angularfire2/database';
 import {AngularFireAuthModule} from 'angularfire2/auth';
+import {AngularFirestore} from "angularfire2/firestore";
 
 export const firebaseConfig = {
   apiKey: "AIzaSyD2OLU1f78pWLsMO-NwIfBJfRRJp4Hlg1k",
@@ -49,6 +50,7 @@ export const firebaseConfig = {
     StatusBar,
     SplashScreen,
     AngularFireDatabase,
+    AngularFirestore,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     BotProvider
   ]
