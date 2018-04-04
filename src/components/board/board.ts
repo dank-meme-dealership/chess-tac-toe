cea5950d6ba3517676e3892ca4f67e0fb8de0cac
Warren Sutherland <wsutherland@relias.com>
2018-04-04 15:13:49 -0600

    moving pieces :pogchamp:

--- src/components/board/board.ts
+++ src/components/board/board.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'board',
@@ -7,9 +7,12 @@ import {Component} from '@angular/core';
 export class BoardComponent {
 
   boardState: string[][];
+  selected: Object;
+  moveTo: Object;
 
   constructor() {
     console.log('Hello BoardComponent Component');
+    this.selected = null;
     this.boardState = [
       ['wr', 'wp', 'wk', 'wb'],
       ['', '', '', ''],
@@ -21,7 +24,30 @@ export class BoardComponent {
   }
 
   onClick(x, y): void {
-    this.boardState[x][y] = 'wr';
+    //if nothing is selected yet and we click a spot with a piece, update this.selected
+    if (this.selected === null && this.boardState[x][y] !== '') {
+      this.selected = {
+        xS: x,
+        yS: y,
+        piece: this.boardState[x][y]
+      };
+    } else if(this.selected !== null){ // if we already have a piece selected then
+      this.moveTo = {
+        xS: x,
+        yS: y,
+        piece: this.boardState[x][y]
+      }
+      if (this.moveTo.piece !==''){
+
+      } else {
+        this.boardState[x][y] = this.selected.piece;
+        this.boardState[this.selected.xS][this.selected.yS] = '';
+      }
+      this.selected = null;
+      this.moveTo = null;
+    }
+
+
   }
 
 }
