66e41856bd24a1de10675b29619ec70a4dcfcea6
Matttaylor8910 <taylor.matt777@gmail.com>
2018-04-05 07:56:30 -0600

    Style

--- src/components/board/board.ts
+++ src/components/board/board.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'board',
@@ -24,34 +24,35 @@ export class BoardComponent {
   }
 
   onClick(x, y): void {
-    //if nothing is selected yet and we click a spot with a piece, update this.selected
+    // if nothing is selected yet and we click a spot with a piece, update this.selected
     if (this.selected === null && this.boardState[x][y] !== '') {
       this.selected = {
         xS: x,
         yS: y,
         piece: this.boardState[x][y]
       };
-    } else if(this.selected !== null){ // if we already have a piece selected then
+    } else if (this.selected !== null) { // if we already have a piece selected then
       this.moveTo = {
         xS: x,
         yS: y,
         piece: this.boardState[x][y]
-      }
-      if (this.moveTo.piece !==''){
-        //check to see if you are moving out of a tray. if so do nothing. you cannot capture a piece while placing
-        if(this.selected.xS === 0 || this.selected.xS === 5){
+      };
+      if (this.moveTo.piece !== '') {
+        // check to see if you are moving out of a tray. if so do nothing.
+        // you cannot capture a piece while placing
+        if (this.selected.xS === 0 || this.selected.xS === 5) {
           return;
         }
 
-        if(this.selected.piece[0]!== this.moveTo.piece[0]){
-          var playerArray;
-          if (this.moveTo.piece[0] =='b'){
+        if (this.selected.piece[0] !== this.moveTo.piece[0]) {
+          let playerArray;
+          if (this.moveTo.piece[0] == 'b') {
             playerArray = 5;
           } else {
             playerArray = 0;
           }
-          var c = 0;
-          while(this.boardState[playerArray][c]!==''){
+          let c = 0;
+          while (this.boardState[playerArray][c] !== '') {
             c++;
           }
           this.boardState[x][y] = this.selected.piece;
