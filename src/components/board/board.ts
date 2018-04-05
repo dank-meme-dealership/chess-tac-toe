ce48b04f6a665dbc35d7788fda7e296a3a31e03f
Warren Sutherland <wsutherland@relias.com>
2018-04-05 13:39:31 -0600

    testing board styling

--- src/components/board/board.ts
+++ src/components/board/board.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import { ChessProvider} from '../../providers/chess/chess';
 
 @Component({
   selector: 'board',
@@ -9,9 +10,10 @@ export class BoardComponent {
   boardState: string[][];
   selected: any;
   moveTo: any;
+  moves: any;
 
-  constructor() {
-    console.log('Hello BoardComponent Component');
+  constructor(public chessProvider: ChessProvider) {
+    console.log('Hello BoardComponent');
     this.selected = null;
     this.boardState = [
       ['wr', 'wpd', 'wk', 'wb'],
@@ -31,43 +33,47 @@ export class BoardComponent {
         yS: y,
         piece: this.boardState[x][y]
       };
-    } else if (this.selected !== null) { // if we already have a piece selected then
-      this.moveTo = {
-        xS: x,
-        yS: y,
-        piece: this.boardState[x][y]
-      };
-      if (this.moveTo.piece !== '') {
-        // check to see if you are moving out of a tray. if so do nothing.
-        // you cannot capture a piece while placing
-        if (this.selected.xS === 0 || this.selected.xS === 5) {
-          return;
-        }
+      this.moves = this.chessProvider.getValidMoves(this.boardState,this.selected);
+      highlightMoves();
+    } 
+    // else if (this.selected !== null) { // if we already have a piece selected then
+    //   this.moveTo = {
+    //     xS: x,
+    //     yS: y,
+    //     piece: this.boardState[x][y]
+    //   };
+    //   if (this.moveTo.piece !== '') {
+    //     // check to see if you are moving out of a tray. if so do nothing.
+    //     // you cannot capture a piece while placing
+    //     if (this.selected.xS === 0 || this.selected.xS === 5) {
+    //       return;
+    //     }
 
-        if (this.selected.piece[0] !== this.moveTo.piece[0]) {
-          let playerArray;
-          if (this.moveTo.piece[0] == 'b') {
-            playerArray = 5;
-          } else {
-            playerArray = 0;
-          }
-          let c = 0;
-          while (this.boardState[playerArray][c] !== '') {
-            c++;
-          }
-          this.boardState[x][y] = this.selected.piece;
-          this.boardState[this.selected.xS][this.selected.yS] = '';
-          this.boardState[playerArray][c] = this.moveTo.piece;
-        }
-      } else {
-        this.boardState[x][y] = this.selected.piece;
-        this.boardState[this.selected.xS][this.selected.yS] = '';
-      }
-      this.selected = null;
-      this.moveTo = null;
-    }
-
-
+    //     if (this.selected.piece[0] !== this.moveTo.piece[0]) {
+    //       let playerArray;
+    //       if (this.moveTo.piece[0] == 'b') {
+    //         playerArray = 5;
+    //       } else {
+    //         playerArray = 0;
+    //       }
+    //       let c = 0;
+    //       while (this.boardState[playerArray][c] !== '') {
+    //         c++;
+    //       }
+    //       this.boardState[x][y] = this.selected.piece;
+    //       this.boardState[this.selected.xS][this.selected.yS] = '';
+    //       this.boardState[playerArray][c] = this.moveTo.piece;
+    //     }
+    //   } else {
+    //     this.boardState[x][y] = this.selected.piece;
+    //     this.boardState[this.selected.xS][this.selected.yS] = '';
+    //   }
+    //   this.selected = null;
+    //   this.moveTo = null;
+    // }
   }
 
+  highlightMoves(){
+    
+  }
 }
--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
 import {BoardComponent} from './board/board';
 import {CommonModule} from '@angular/common';
+import { ChessProvider } from '../providers/chess/chess';
 
 @NgModule({
   declarations: [BoardComponent],
   imports: [CommonModule],
-  exports: [BoardComponent]
+  exports: [BoardComponent],
+  providers:[ChessProvider]
 })
 export class ComponentsModule {
 }
