1cb0ff88b9273a0559aad2feaa49fd4cd23d26f5
Matttaylor8910 <taylor.matt777@gmail.com>
2018-04-05 14:51:17 -0600

    v1 of a very fragile matchmaking algorithm

--- src/pages/home/play-game-modal.ts
+++ src/pages/home/play-game-modal.ts
@@ -45,12 +45,12 @@ export class PlayGameModal {
     let name = this.params.get('name');
     let uid = localStorage.getItem('uid');
     if (uid) {
-      this.joinQueue({name: name, uid: uid}, type);
+      this.joinQueue({name: name, id: uid}, type);
     }
     else {
       this.addUser(name).then(function (uid) {
         localStorage.setItem('uid', uid);
-        this.joinQueue({name: name, uid: uid}, type);
+        this.joinQueue({name: name, id: uid}, type);
       }.bind(this));
     }
   }
@@ -68,18 +68,10 @@ export class PlayGameModal {
       this.navCtrl.push(QueuePage, {player: player, type: type});
       this.viewCtrl.dismiss();
     }
-    // otherwise we need to add them to the queue and send them on their way with that id
+    // otherwise we need send them to the queue page to have a match made
     else {
-      this.afs.collection<any>('queue')
-        .add({
-          player: player,
-          queueTimestamp: moment().unix(),
-        })
-        .then(queue => {
-          // nav to this game
-          this.navCtrl.push(QueuePage, {player: player, queueId: queue.id});
-          this.viewCtrl.dismiss();
-        });
+      this.navCtrl.push(QueuePage, {player: player, type: type});
+      this.viewCtrl.dismiss();
     }
   }
 }
--- src/pages/queue/queue.ts
+++ src/pages/queue/queue.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {Game} from "../home/play-game-modal";
 import {GameplayPage} from "../gameplay/gameplay";
 import {AngularFirestore} from 'angularfire2/firestore';
+import { Subject } from 'rxjs/Subject';
 
 const moment = require("moment");
 
@@ -12,36 +13,119 @@ const moment = require("moment");
   templateUrl: 'queue.html',
 })
 export class QueuePage {
+  private gameCollection;
+  private queueCollection;
+  private queue;
+  private ngUnsubscribe: Subject<void> = new Subject();
+  private busy;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private afs: AngularFirestore) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad QueuePage');
+    // ensure this class doesn't think we're busy
+    this.busy = false;
+    let player = this.navParams.data.player;
+
+    // private games just start and are handled in the gameplay page
     if (this.navParams.data.type === 'private') {
-      this.joinGame(this.navParams.data.player, true);
+      this.createGame([player], true).then(game => {
+        // nav to this game, nothing to delete
+        this.joinGame(game.id, null);
+      });;
+    }
+    // matchmaking games join a queue
+    else {
+      // sort the collection ascending by queueTimestamp
+      this.queueCollection = this.afs.collection<any>('queue', 
+        ref => ref.orderBy('queueTimestamp', 'asc')
+      );
+
+      // add this player to the queue
+      this.queueCollection.add({
+        player: player,
+        queueTimestamp: moment().unix(),
+      });
+      // listen for changes to the list and determine queue position
+      this.queue = this.queueCollection.snapshotChanges()
+        .takeUntil(this.ngUnsubscribe)
+        .subscribe(queue => {
+          this.determineQueuePosition(queue, player);
+        });
     }
   }
 
-  joinGame(player: any, privateGame: boolean) {
-    this.afs.collection<Game>('games')
+  determineQueuePosition(queue: any, player: any) {
+    if (this.busy) return;
+
+    // remove queue items that are already done
+    queue = queue.filter(queueEntry => queueEntry.payload.doc.data().done !== true);
+    let queueGame = this.getQueueGame(queue, player);
+
+    if (queueGame) {
+      this.busy = true;
+      this.joinGame(queueGame.gameId, queueGame.queueId);
+    }
+    
+    // if gameId returned as null, it means you haven't been invited to a game yet
+    else {
+      // filter the queue down to just people without games to join
+      let filtered = queue.filter(queueEntry => queueEntry.gameId === undefined);
+
+      // if there is a queue length > 1, figure out if you need to do anything
+      if (filtered.length > 1) {
+        let firstInQueue = filtered[0].payload.doc.data();
+
+        // only the first player in the queue will do game creation
+        if (firstInQueue.player.id === player.id) {
+          this.busy = true;
+          let players = [];
+          players.push(filtered[0].payload.doc.data().player);
+          players.push(filtered[1].payload.doc.data().player);
+
+          this.createGame(players, false).then(game => {
+            // put the gameId on both players
+            this.afs.doc<any>('queue/' + filtered[0].payload.doc.id).update({gameId: game.id});
+            this.afs.doc<any>('queue/' + filtered[1].payload.doc.id).update({gameId: game.id});
+          });
+
+          this.busy = false;
+        }
+      }
+    }
+  }
+
+  getQueueGame(queue: any, player: any) {
+    for (let entry of queue) {
+      let queuedEntry = entry.payload.doc.data();
+      if (queuedEntry.player.id === player.id && queuedEntry.gameId) {
+        return {
+          gameId: queuedEntry.gameId,
+          queueId: entry.payload.doc.id
+        };
+      }
+    }
+    return null;
+  }
+
+  createGame(players: any[], privateGame: boolean) {
+    return this.afs.collection<Game>('games')
       .add({
-        players: [player],
+        players: players,
         turns: [],
         queueTimestamp: moment().unix(),
         privateGame: privateGame,
-      })
-      .then(game => {
-        // nav to this game
-        this.navCtrl.push(GameplayPage, {gameId: game.id});
       });
   }
 
-  invitePlayerToGame(gameId: string) {
-    // TODO: Invite a player in the queue to the game
-    // once they've responded that they've seen it, 
-    // remove both players from the queue and they will
-    // both transition into the game
-  }
+  joinGame(gameId: string, queueToDelete: string) {
+    // if this game is being joined by a player from the queue,
+    // that user's queueId needs to be removed
+    if (queueToDelete) {
+      this.afs.doc<any>('queue/' + queueToDelete).update({done: true});
+    }
 
+    this.navCtrl.pop(); // remove queue from history stack
+    this.navCtrl.push(GameplayPage, {gameId: gameId});
+  }
 }
