55c3140042b6250f3355f13a3a1f0015d97778f3
jweaver <jweaver@relias.com>
2018-04-05 10:40:05 -0600

    pawn done

--- src/providers/chess/chess.ts
+++ src/providers/chess/chess.ts
@@ -52,27 +52,39 @@ export class ChessProvider {
     var color = selected.piece[0];
     var direction = selected.piece[2];
     var x, y;
+
     if (direction === 'u') {
       x = selected.xS - 1;
     } else {
       x = selected.xS + 1;
     }
 
-    for (y = selected.yS - 1; y <= selected.yS + 1; y++) {
-      if (!this.moveIsOnBoard(x,y)) {
-        return [];
-      }
-    }
+    //forward
+    y = selected.yS;
+    let possible = this.checkSpace(x, y,color,board);
+    if(possible) legalMoves.push(possible);
+
+    //forward left/right
+    y=selected.yS+1;
+    possible = this.checkSpace(x,y,color,board)
+    if(possible && possible.o) legalMoves.push(possible);
+
+    //forward left/right
+    y=selected.yS-1;
+    possible = this.checkSpace(x,y,color,board)
+    if(possible && possible.o) legalMoves.push(possible);
+
+    return legalMoves;
   }
 
   checkSpace(x, y, color, board) {
-
+    if(!this.moveIsOnBoard(x,y)) return null;
     if (board[x][y] === '') {
       return { x: x, y: y, o:true}; //pass back space as empty
     } else if (board[x][y][0] !== color) {
       return { x: x, y: y, o:false}; //pass back space as occupied if it has opponent
     }
-    return []; // otherwise pass back not a valid move;
+    return null; // otherwise pass back not a valid move;
   }
 
   moveIsOnBoard(x,y){
