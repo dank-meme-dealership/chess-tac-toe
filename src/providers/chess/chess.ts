5dd08b974a46a8a3ceb6fda269ed64dd2a3d6832
Warren Sutherland <wsutherland@relias.com>
2018-04-05 10:15:43 -0600

    chess logic v1

--- src/providers/chess/chess.ts
+++ src/providers/chess/chess.ts
@@ -1,5 +1,5 @@
-import {HttpClient} from '@angular/common/http';
-import {Injectable} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 
 export interface Move {
   playerId: number;
@@ -48,7 +48,30 @@ export class ChessProvider {
   }
 
   getPawnLegalMove(board, selected) {
+    var legalMoves = [];
     var color = selected.piece[0];
     var direction = selected.piece[2];
+    var x, y;
+    if (direction === 'u') {
+      x = selected.xS - 1;
+    } else {
+      x = selected.xS + 1;
+    }
+
+    for (y = selected.yS - 1; y <= selected.yS + 1; y++) {
+      if (x === 0 || x === 5 || y === -1 || y == 4) {
+        return [];
+      }
+    }
+  }
+
+  checkSpace(x, y, color, board) {
+    
+    if (board[x][y] === '') {
+      return { x: x, y: y, o:true}; //pass back space as empty
+    } else if (board[x][y][0] !== color) {
+      return { x: x, y: y, o:false}; //pass back space as occupied if it has opponent
+    }
+    return []; // otherwise pass back not a valid move;
   }
 }
