bd71c1402b1499617af13be6772ddd938b251b03
Matttaylor8910 <taylor.matt777@gmail.com>
2018-04-05 07:57:35 -0600

    More style

--- src/providers/chess/chess.ts
+++ src/providers/chess/chess.ts
@@ -1,12 +1,6 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
 
-/*
-  Generated class for the ChessProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class ChessProvider {
 
@@ -14,41 +8,41 @@ export class ChessProvider {
     console.log('Hello ChessProvider Provider');
   }
 
-  getValidMoves(board,selected){
+  getValidMoves(board, selected) {
     var pieceType = selected.piece[1];
-    switch(pieceType){
+    switch (pieceType) {
       case 'r': {
-        return this.getRookLegalMove(board,selected);
+        return this.getRookLegalMove(board, selected);
       }
       case 'b': {
-        this.getBishopLegalMove(board,selected);
+        this.getBishopLegalMove(board, selected);
         break;
       }
       case 'k': {
-        this.getKnightLegalMove(board,selected);
+        this.getKnightLegalMove(board, selected);
         break;
       }
       case 'p': {
-        this.getPawnLegalMove(board,selected);
+        this.getPawnLegalMove(board, selected);
         break;
       }
     }
   }
 
 
-  getRookLegalMove(board,selected){
+  getRookLegalMove(board, selected) {
 
   }
 
-  getBishopLegalMove(board,selected){
+  getBishopLegalMove(board, selected) {
 
   }
 
-  getKnightLegalMove(board,selected){
+  getKnightLegalMove(board, selected) {
 
   }
 
-  getPawnLegalMove(board,selected){
+  getPawnLegalMove(board, selected) {
     var color = selected.piece[0];
     var direction = selected.piece[2];
   }
