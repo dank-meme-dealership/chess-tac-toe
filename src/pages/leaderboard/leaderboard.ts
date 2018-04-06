2a2cedf157b9e849c093dc1d1491146659ec6d0b
Matttaylor8910 <taylor.matt777@gmail.com>
2018-04-06 10:11:51 -0600

    Done with leaderboard

--- src/pages/leaderboard/leaderboard.ts
+++ src/pages/leaderboard/leaderboard.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {AngularFirestore} from "angularfire2/firestore";
+import {Subject} from "rxjs/Subject";
+import _ from 'lodash';
 
 @IonicPage()
 @Component({
@@ -7,11 +10,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'leaderboard.html',
 })
 export class LeaderboardPage {
+  private ngUnsubscribe: Subject<void> = new Subject();
+  players;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private afs: AngularFirestore) {
   }
 
   ionViewDidLoad() {
+    this.afs.collection<any>('users')
+      .snapshotChanges()
+      .takeUntil(this.ngUnsubscribe)
+      .subscribe(users => {
+        this.players = _.reverse(_.sortBy(this.filterUsers(users), 'wins'));
+      });
+  }
+
+  filterUsers(users: any) {
+    return (users)
+      .map(function (user) {
+        return user.payload.doc.data();
+      })
+      .filter(function (user) {
+        return user.wins > 0;
+      });
   }
 
 }
